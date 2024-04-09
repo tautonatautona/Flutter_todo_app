6e98e4cfd4a28ee45a0ddb04bdfe34ad65727030
tautonatautona <ttautona09@gmail.com>
2024-04-09 14:29:55 +0200

    android sdk

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -48,7 +48,35 @@ class Home extends StatelessWidget{
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: Row(children: [Expanded(child: child)],
+            child: Row(children: [
+              Expanded(child: Container(margin:  EdgeInsets.only(
+                bottom: 20,
+                right: 20,
+                left: 20,
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 0.0),
+                  blurRadius: 10.0,
+                  spreadRadius: 0.0,
+                ),],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Add new toto item',
+                  border: InputBorder.none,
+                ),
+              ),
+              )
+              )
+              ],
             ),
           )
         ],
